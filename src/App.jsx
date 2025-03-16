76bf0335efcceb515e56615186577eedef15803e
Jordan <tomegahjordan81@gmail.com>
2025-03-16 00:56:09 +0000

    definition de la classe AppError , et gestion des erreurs

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,22 @@
-const App = () =>{
-    return (
-        <div>
-            App
+const App = () => {
+  return (
+    <main>
+      <div className="pattern">
+        <div className="wrapper">
+          <header>
+            <h1>
+              Find <span className="text-gradient">Movies</span> You'll Enjoy
+              without the Hassle
+            </h1>
+          </header>
+          <p>
+            Search
+          </p>
         </div>
-    )
-}
+      </div>
+      <h1></h1>
+    </main>
+  );
+};
 
 export default App;
-
